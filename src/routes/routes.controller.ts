e92122b6f11f108b4f46fb2a078b7432324271db
Paloma Rehecho <paloma_re@hotmail.com>
2024-08-23 15:13:33 +0100

    updated format

--- src/routes/routes.controller.ts
+++ src/routes/routes.controller.ts
@@ -1,42 +1,40 @@
-import { Controller, Get, Query, Res } from '@nestjs/common';
+import { Body, Controller, Post, Res } from '@nestjs/common';
 import { RoutesService } from './routes.service';
 import { Response } from 'express';
+
+interface RoutesRequest {
+    startLat: string,
+    startLng: string,
+    endLat: string,
+    endLng: string,
+    waypoints: Waypoint[]
+}
+
+interface Waypoint {
+    lat: string,
+    lng: string
+}
 
 @Controller('routes')
 export class RoutesController {
     constructor(private readonly routesService: RoutesService) { }
 
-
-    //TODO: pasar a método post
-    @Get()
+    @Post()
     async getRoute(
         @Res() res: Response,
-        @Query('startLat') startLat: string,
-        @Query('startLng') startLng: string,
-        @Query('endLat') endLat: string,
-        @Query('endLng') endLng: string,
-        @Query('waypointLat') waypointLat: string | string[],
-        @Query('waypointLng') waypointLng: string | string[]
+        @Body() body: RoutesRequest
     ): Promise<Response> {
 
         try {
+            const {
+                startLat,
+                startLng,
+                endLat,
+                endLng,
+                waypoints
+            } = body
 
-            const latArray = Array.isArray(waypointLat) ? waypointLat : [waypointLat];
-            const lngArray = Array.isArray(waypointLng) ? waypointLng : [waypointLng];
-            // Lógica para la lista de waypoints
-            // Asegurarse de que latArray y lngArray tengan el mismo tamaño
-            if (latArray.length !== lngArray.length) {
-                throw new Error('Los arrays waypointLat y waypointLng deben tener la misma longitud.');
-            }
-
-            // Crear el array de waypoints
-            const waypoints = latArray.map((lat, index) => ({
-                lat,
-                lng: lngArray[index]
-            }));
-
-            // Lógica para obtener la polyline entre A y B pasando por C
-            const polyline = await this.routesService.getPolyline({ startLat, startLng, endLat, endLng, waypoints });
+            const polyline = await this.routesService.getPolyline({ startLat, startLng, waypoints, endLat, endLng });
             return res.status(200).send(polyline);
         } catch (error) {
             console.log('new error in query', error)
